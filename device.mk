3983b12ea3a588c16af09e31bfd8b912108a53ed
lllgts <wuer031@126.com>
2023-01-14 09:16:36 +0000

    Remove lineage stuff

--- device.mk
+++ device.mk
@@ -361,8 +361,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 	
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay/frameworks/base/packages/overlays/NoCutoutOverlay
